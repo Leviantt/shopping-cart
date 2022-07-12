4be680f126a79d0f78d5f2b3ba4246a37eda9823
Lev Nefedov <nefedov.lewa@gmail.com>
2022-07-13 00:04:38 +0400

    added basic styling to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Store from './pages/Store';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="container">
+    <>
+    <Navbar />
+    <div className="container body-container">
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/store' element={<Store />}></Route>
         <Route path='/about' element={<About />}></Route>
       </Routes>
     </div>
+    </>
   )
 }
 
